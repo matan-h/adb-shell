c6aeaaa3ad21d348fcf7d6842a12443dc6138116
matan-h <matan.honig2@gmail.com>
2024-04-15 12:28:08 +0300

    Add PowerShell explanation (suggested on reddit)

--- adb-shell.ps1
+++ adb-shell.ps1
@@ -1,6 +1,6 @@
 $STARTUP = "/sdcard/.adb/startup.sh"
 adb shell "[ -f $STARTUP ]" ; 
-if (-not $?){
+if (-not $?){ # $?: command success status (true if the last command was successful)
     Write-Output "file not found, push with adb"
     adb push "$PSScriptRoot/startup.sh" $STARTUP
 }
