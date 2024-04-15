d73b5042f47fb52bf80b61488adc0c202ec4698b
matan-h <matan.honig2@gmail.com>
2024-04-15 15:39:24 +0300

    make a clear deference between adb shell fail and adb error

--- adb-shell.ps1
+++ adb-shell.ps1
@@ -1,7 +1,10 @@
 $STARTUP = "/sdcard/.adb/startup.sh"
-adb shell "[ -f $STARTUP ]" ; 
-if (-not $?){ # $?: command success status (true if the last command was successful)
+adb shell "[ -f $STARTUP ] || exit 6" ; 
+if ($LastExitCode -eq 6){ # $?: command success status (true if the last command was successful)
     Write-Output "file not found, push with adb"
     adb push "$PSScriptRoot/startup.sh" $STARTUP
+}
+if ($LastExitCode -eq 1){ # adb error
+    exit 1
 }
 adb shell -t "HOME='/sdcard' ENV='$STARTUP' sh -i -o emacs -o vi-tabcomplete"
--- adb-shell.sh
+++ adb-shell.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 STARTUP=/sdcard/.adb/startup.sh
-if ! adb shell "[ -f $STARTUP ]" ; then 
+adb shell "[ -f $STARTUP ] || exit 6";
+last_exit_status=$?
+
+if [ $last_exit_status -eq 6 ] ; then
     echo "file not found, push with adb"
     adb push "$(dirname "$0")/startup.sh" $STARTUP
+
+elif [ $last_exit_status -eq 1 ]; then # adb error
+    exit 1
 fi
 # for most shells, '-o emacs -o vi-tabcomplete' is the default, but for some it isn't.
 adb shell -t "HOME='/sdcard' ENV='$STARTUP' sh -i -o emacs -o vi-tabcomplete"
